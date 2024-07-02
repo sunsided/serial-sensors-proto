ec2bc06fb58e1c3cd33b54861f1200e110dc54d3
Markus Mayer <widemeadows@gmail.com>
2024-07-03 00:39:39 +0200

    Build implementation for types from macro

--- crates/serial-sensors-proto-derive/src/lib.rs
+++ crates/serial-sensors-proto-derive/src/lib.rs
@@ -1,16 +1,16 @@
 // src/lib.rs
 extern crate proc_macro;
 
-use darling::{FromDeriveInput, FromField, FromMeta, FromMetaItem, FromVariant};
+use darling::{FromDeriveInput, FromMeta, FromVariant};
 use proc_macro::TokenStream;
-use proc_macro2::Ident;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Path, Type, Variant};
+use std::collections::HashSet;
+use syn::{parse_macro_input, DeriveInput, Path};
 
 #[derive(Debug, FromMeta)]
 struct SensorAttributes {
     id: u8,
-    data: String,
+    data: Path,
     components: u8,
 }
 
@@ -37,8 +37,11 @@ pub fn derive_runtime_type_information(input: TokenStream) -> TokenStream {
     let name = &version1_data.ident;
 
     let mut sensor_match_arms = Vec::new();
-    let mut field_match_arms: Vec<String> = Vec::new();
-    let mut num_components_match_arms: Vec<u8> = Vec::new();
+    let mut field_match_arms = Vec::new();
+    let mut num_components_match_arms = Vec::new();
+
+    let mut sensor_types = HashSet::new();
+    let mut duplicate_error = None;
 
     if let darling::ast::Data::Enum(variants) = &version1_data.data {
         for variant in variants {
@@ -47,40 +50,49 @@ pub fn derive_runtime_type_information(input: TokenStream) -> TokenStream {
             let field_type = &variant.sensor.data;
             let num_components = variant.sensor.components;
 
+            if !sensor_types.insert(sensor_type) {
+                let variant = variant_name.to_string();
+                duplicate_error = Some(quote! {
+                    compile_error!(concat!("Duplicate sensor type found (", #sensor_type, ") at ", #variant));
+                });
+                break;
+            }
+
             sensor_match_arms.push(quote! {
                 #name::#variant_name(_) => #sensor_type,
             });
 
-            /*
             field_match_arms.push(quote! {
-                #name::#variant_name(_) => crate::types::#field_type::FIELD,
+                #name::#variant_name(_) => #field_type,
             });
+
             num_components_match_arms.push(quote! {
-                #name::#variant_name(_) => crate::types::#field_type::NUM_COMPONENTS,
+                #name::#variant_name(_) => #num_components,
             });
-             */
         }
     }
 
-    let expanded = quote! {
-        impl ::serial_sensors_proto_traits::RuntimeTypeInformation2 for #name {
-            fn sensor_type_id(&self) -> u8 {
-                match self {
-                    #( #sensor_match_arms )*
+    let expanded = if let Some(error) = duplicate_error {
+        error
+    } else {
+        quote! {
+            impl ::serial_sensors_proto_traits::RuntimeTypeInformation2 for #name {
+                fn sensor_type_id(&self) -> u8 {
+                    match self {
+                        #( #sensor_match_arms )*
+                    }
                 }
-            }
 
-            fn value_type(&self) -> ::serial_sensors_proto_traits::ValueType {
-                match self {
-                    _ => todo!(),
-                    #( #field_match_arms )*
+                fn value_type(&self) -> ::serial_sensors_proto_traits::ValueType {
+                    match self {
+                        #( #field_match_arms )*
+                    }
                 }
-            }
 
-            fn num_components(&self) -> u8 {
-                match self {
-                    _ => todo!(),
-                    #( #num_components_match_arms )*
+                fn num_components(&self) -> u8 {
+                    match self {
+                        #( #num_components_match_arms )*
+                    }
                 }
             }
         }
--- crates/serial-sensors-proto/src/test.rs
+++ crates/serial-sensors-proto/src/test.rs
@@ -1,23 +1,35 @@
 use serial_sensors_proto_derive::SerialSensors;
+use serial_sensors_proto_traits::ValueType;
 
 /// Data formats.
 #[derive(Debug, Clone, PartialEq, SerialSensors)]
 pub enum Test {
-    #[sensor(id = 0x01, data = "UInt32", components = 1)]
-    ProtocolVersions(crate::types::ProtocolVersion),
-    #[sensor(id = 0x02, data = "UInt32", components = 1)]
+    /// The system clock frequency, expressed in Hertz (Hz).
+    #[sensor(id = 0x2, data = ValueType::UInt32, components = 1)]
     SystemClockFrequency(crate::types::SystemClockFrequency),
-    #[sensor(id = 0x42, data = "SInt16", components = 3)]
+
+    /// A sensor that measures the gravity vector, typically expressed in "g".
+    #[sensor(id = 0x42, data = ValueType::SInt16, components = 3)]
     AccelerometerI16(crate::types::AccelerometerI16),
-    #[sensor(id = 0x43, data = "SInt16", components = 3)]
+
+    /// A sensor that measures magnetic field strength, typically expressed in units auf Milli-Gauss (mG).
+    #[sensor(id = 0x43, data = ValueType::SInt16, components = 3)]
     MagnetometerI16(crate::types::MagnetometerI16),
-    #[sensor(id = 0x44, data = "SInt16", components = 1)]
+
+    /// A sensor that measures temperature, typically expressed in °C.
+    #[sensor(id = 0x44, data = ValueType::SInt16, components = 1)]
     TemperatureI16(crate::types::TemperatureI16),
-    #[sensor(id = 0x45, data = "SInt16", components = 1)]
+
+    /// A sensor that measures angular acceleration, typically expressed in degrees/second.
+    #[sensor(id = 0x45, data = ValueType::SInt16, components = 1)]
     GyroscopeI16(crate::types::GyroscopeI16),
-    #[sensor(id = 0xF0, data = "Float32", components = 3)]
+
+    /// Euler angles, in radians.
+    #[sensor(id = 0xF0, data = ValueType::Float32, components = 3)]
     EulerAnglesF32(crate::types::EulerAnglesF32),
-    #[sensor(id = 0xF1, data = "Float32", components = 4)]
+
+    /// An orientation quaternion.
+    #[sensor(id = 0xF1, data = ValueType::Float32, components = 4)]
     OrientationQuaternionF32(crate::types::OrientationQuaternionF32),
 }
 
@@ -33,5 +45,7 @@ mod tests {
         let instance =
             Test::AccelerometerI16(AccelerometerI16::new(Vector3Data { x: 1, y: -2, z: 3 }));
         assert_eq!(instance.sensor_type_id(), 0x42);
+        assert_eq!(instance.value_type(), ValueType::SInt16);
+        assert_eq!(instance.num_components(), 3);
     }
 }
